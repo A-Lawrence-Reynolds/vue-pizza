30e363aa54e9b9c4455944488af519a2c5f05290
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-04-03 17:13:08 -0700

    tarted working on vue project

---
+++ src/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+
+  <header>
+
+    <div class='header_logo'>
+      <img
+        src="../assets/planet.png"
+        alt="pizza planet"
+      >
+      <h1 href='/'>PIZZA PLANET </h1>
+
+    </div>
+    <nav>
+      <ul>
+        <il>
+          <a href='/'>HOME</a>
+        </il>
+        <span>
+          ~
+        </span>
+        <il>
+          <a href='/menu'>MENU</a>
+        </il>
+        <span>
+          ~
+        </span>
+        <il>
+          <a href='/contact'>CONTACT</a>
+        </il>
+        <span>
+          ~
+        </span>
+        <il>
+          <a href='/about'>ABOUT US</a>
+        </il>
+
+      </ul>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  name: "appHeader"
+};
+</script>
+<style scoped>
+h1 {
+  font-size: 1.3em;
+}
+
+.header_logo {
+  background-image: linear-gradient(
+    45deg,
+    #d85226 25%,
+    #f79e38 25% 50%,
+    #d85226 50% 75%,
+    #d85226 75%,
+    #d85226 100%
+  );
+  background-size: 80px 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
+}
+.header_logo img {
+  max-width: 25vw;
+}
+ul {
+  padding: 10px 0;
+}
+li {
+    display:inline-block;
+}
+@media screen and (min-width: 900px) {
+  .header_logo img {
+  max-width: 25%;
+}
+}
+</style>
