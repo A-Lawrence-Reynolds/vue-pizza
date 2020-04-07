0f960bb04c09498d4e40916e3bd2e7945d2ccee4
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-04-07 14:04:33 -0700

    added pizza data temp untill fire base is set up

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
   <div id="app">
   <app-header/>
   <Home/>
+  <Menu/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import Home from './components/Home'
+import Menu from './components/Menu'
 export default {
   name: 'App',
   components: {
+  Menu,
   Home,
   appHeader: Header
   }
 }
 </script>
 
-<style>
-
-</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="menu_wrapper">
+    <!--menu -->
+    <div class="menu">
+      <h3>
+        ◊ Authentic handmade pizza ◊
+      </h3>
+      <table
+        v-for="item in getMenuItems"
+        :key="item.name"
+      >
+        <tbody>
+            <!-- name and description -->
+          <tr>
+            <td>
+              <strong>
+                {{item.name}}
+              </strong>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <small>
+                {{item.description}}
+              </small>
+            </td>
+          </tr>
+          <!-- price and size  -->
+          <tr v-for="(option , index) in item.options" :key="index">
+            <td>{{option.size}}"</td>
+            <td>${{option.price}}</td>
+            <td>
+              <button
+                type="button"
+                class='btn_green'
+              >+</button>
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      getMenuItems: {
+        1: {
+          name: "Margherita",
+          description: "A delicious tomato based pizza topped with mozzarella",
+          options: [
+            {
+              size: 9,
+              price: 6.95
+            },
+            {
+              size: 12,
+              price: 10.95
+            }
+          ]
+        },
+        2: {
+          name: "Pepperoni",
+          description:
+            "A delicious tomato based pizza topped with mozzarella and pepperoni",
+          options: [
+            {
+              size: 9,
+              price: 7.95
+            },
+            {
+              size: 12,
+              price: 12.95
+            }
+          ]
+        },
+        3: {
+          name: "Ham and Pineapple",
+          description:
+            "A delicious tomato based pizza topped with mozzarella, ham and pineapple",
+          options: [
+            {
+              size: 9,
+              price: 7.95
+            },
+            {
+              size: 12,
+              price: 12.95
+            }
+          ]
+        }
+      }
+    };
+  }
+};
+</script>
+
+
+
+
+<style scoped>
+h3 {
+  text-align: center;
+}
+.menu_wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.menu {
+  background: #f1e6da;
+  border-radius: 3px;
+  height: 100vh;
+  padding: 10px;
+  margin: 10px;
+}
+@media screen and (min-width: 900px) {
+  .menu_wrapper {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .menu {
+    width: 65vw;
+  }
+}
+</style>
